259ef6a85c4d0bb136792faff721a90a7023f99b
Lucas García <lucas@codeccoop.org>
2023-09-12 14:32:26 +0200

    feat: add searcher shortcode

--- inc/ajax/search.php
+++ inc/ajax/search.php
@@ -1,6 +1,43 @@
 <?php
 
 add_action('wp_ajax_waf_search', 'waf_ajax_waf_search');
+add_action('wp_ajax_nopriv_waf_search', 'waf_ajax_waf_search');
 function waf_ajax_waf_search()
 {
+    check_ajax_referer('waf-searcher', 'nonce');
+
+    $post_type = $_GET['post_type'];
+    $search_term = $_GET['search_term'];
+
+    $title_like_query = array(
+        'cc_search_term' => $search_term,
+        'post_type' => $post_type,
+        'post_status' => 'publish',
+    );
+
+    add_filter('posts_where', 'waf_posts_like_where', 10, 2);
+    $query = new WP_Query($title_like_query);
+    remove_filter('posts_where', 'waf_posts_like_where', 10);
+
+    $html = '';
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_id = get_the_ID();
+        $html .= apply_filters('waf_template', apply_filters("waf_template_{$post_type}", '', $post_id), $post_id);
+    }
+
+    echo $html;
+    wp_die();
+}
+
+function waf_posts_like_where($where, $wp_query)
+{
+    global $wpdb;
+    if ($search_term = $wp_query->get('cc_search_term')) {
+        $where .= ' AND (LOWER(' . $wpdb->posts . '.post_title) LIKE LOWER(\'%' . $wpdb->esc_like($search_term) . '%\')';
+        $where .= ' OR LOWER(' . $wpdb->posts . '.post_content) LIKE LOWER(\'%' . $wpdb->esc_like($search_term) . '%\')';
+        $where .= ' OR LOWER(' . $wpdb->posts . '.post_excerpt) LIKE LOWER(\'%' . $wpdb->esc_like($search_term) . '%\'))';
+    }
+    echo substr($where, 57);
+    return substr($where, 57);
 }
--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -11,7 +11,9 @@ function waf_tax_filter($atts = [])
         return '[' . $e->getMessage() . ']';
     }
 
-    $tax_names = explode(',', $atts['taxonomies']);
+    $tax_names = array_map(function ($tax_name) {
+        return trim($tax_name, ' ');
+    }, explode(',', $atts['taxonomies']));
     $taxonomies = array_values(array_filter(get_taxonomies([], 'objects'), function ($tax) use ($tax_names) {
         return in_array($tax->name, $tax_names);
     }));
@@ -19,7 +21,7 @@ function waf_tax_filter($atts = [])
     if (!wp_script_is('waf-tax-filter')) wp_enqueue_script('waf-tax-filter');
 
     ob_start(); ?>
-    <div class="waf-filter-form" for="<?= $atts['el']; ?>">
+    <div class="waf-filter-form" aria-controls="<?= $atts['el']; ?>">
         <?php foreach ($taxonomies as $tax) : ?>
             <div class="waf-filter-field" data-type="select" id="<?= $tax->name; ?>">
                 <label for="<?= $tax->name; ?>"><?= $tax->label ?></label>
@@ -44,4 +46,27 @@ function waf_tax_filter($atts = [])
 add_shortcode('waf_tax_filter', 'waf_tax_filter');
 function waf_searcher()
 {
+    try {
+        if (!isset($atts['el'])) throw new Exception('Em falta el selector css del contingut');
+        if (!isset($atts['post_type'])) $atts['post_type'] = 'post';
+    } catch (Exception $e) {
+        return '[' . $e->getMessage() . ']';
+    }
+
+    ob_start(); ?>
+    <div class="waf-filter-form" aria-controls="<?= $atts['el']; ?>">
+        <div class="waf-filter-field" data-type="text" id="search_term">
+            <label for="search-pattern"><?= __('search', 'wp-ajax-filters'); ?></label>
+            <input name="search_term" type="text" />
+        </div>
+        <div class="waf-filter-field" data-type="hidden" style="display: none">
+            <input type="text" name="post_type" value="<?= $atts['post_type']; ?>" />
+        </div>
+        <div class="waf-filter-field" data-type="submit">
+            <input type="submit" value="<?= __('submit', 'wp-ajax-filters'); ?>" />
+        </div>
+    </div>
+<?php
+
+    return ob_get_clean();
 }
